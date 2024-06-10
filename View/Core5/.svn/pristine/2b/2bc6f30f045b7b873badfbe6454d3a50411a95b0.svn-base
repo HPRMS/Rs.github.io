0afc6c3366338559abcbd1aaa37d79a856203500
Andrie <jurig.jarian1002@gmail.com>
2024-06-10 16:17:27 +0700

    remove folder view

--- View/Core5/.svn/pristine/2b/2bc6f30f045b7b873badfbe6454d3a50411a95b0.svn-base
+++
@@ -1,9 +0,0 @@
-/*
-用作模板
-*/
-sjs.using("Core5.Container")
-.define(function(container){
-	return {
-		$extend:container
-	};
-});
--- View/Core5/Widget/Table/RowKey.js
+++
@@ -1,7 +0,0 @@
-﻿/*
-將row的一個或多個欄位值，以attr方式加入到tr的tag中
-*/
-sjs.using("Core5.Widget.Table.GenRowKey")
-.define(function(genRowKey){
-    return genRowKey("row");
-});
